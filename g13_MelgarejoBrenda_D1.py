c9bbc6814b698d6d700a29bfd25958346d83101e
Brenda Melgarejo <brendal_la22@hotmail.com>
2021-12-04 15:58:43 -0300

    Subiendo mi primer archivo

---
+++ g13_MelgarejoBrenda_D1.py
@@ -0,0 +1,8 @@
+print ("Ingrese su nombre y edad")
+a = (input("Ingrese su nombre "))
+b = int(input("Ingrese su edad "))
+if b >= 16:
+  print ((a) + " puede votar")
+else:
+  print ((a) + " debe esperar para votar")
+ 
